e0fbd1247cbe913bb32dc28c4c1174c8ca16eaa3
LarysaTodorova <lorageo33@gmail.com>
2025-05-10 22:30:57 +0300

    commit

--- carDataProject/src/test/java/com/example/cardataproject/controller/ProducerControllerTest.java
+++ carDataProject/src/test/java/com/example/cardataproject/controller/ProducerControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.cardataproject.controller;
 
+import com.example.cardataproject.service.producerService.AddProducerService;
+import com.example.cardataproject.service.producerService.DeleteProducerService;
+import com.example.cardataproject.service.producerService.FindProducerService;
+import com.example.cardataproject.service.producerService.UpdateProducerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -14,8 +18,17 @@ class ProducerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
-    private ProducerController producerController;
+    @MockBean
+    private AddProducerService addProducerService;
+
+    @MockBean
+    private DeleteProducerService deleteProducerService;
+
+    @MockBean
+    private FindProducerService findProducerService;
+
+    @MockBean
+    private UpdateProducerService updateProducerService;
 
     @Test
     void createNewProducer() {
