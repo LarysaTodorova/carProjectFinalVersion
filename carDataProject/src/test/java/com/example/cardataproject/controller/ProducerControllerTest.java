e5c9ffb11566f30fb68cca997e7e1f26896b604d
LarysaTodorova <lorageo33@gmail.com>
2025-05-10 23:05:36 +0300

    commit

--- carDataProject/src/test/java/com/example/cardataproject/controller/ProducerControllerTest.java
+++ carDataProject/src/test/java/com/example/cardataproject/controller/ProducerControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.cardataproject.controller;
 
+import com.example.cardataproject.dto.producerDTO.ProducerRequest;
+import com.example.cardataproject.dto.producerDTO.ProducerResponse;
 import com.example.cardataproject.service.producerService.AddProducerService;
 import com.example.cardataproject.service.producerService.DeleteProducerService;
 import com.example.cardataproject.service.producerService.FindProducerService;
@@ -8,8 +10,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(ProducerController.class)
@@ -31,7 +40,29 @@ class ProducerControllerTest {
     private UpdateProducerService updateProducerService;
 
     @Test
-    void createNewProducer() {
+    void createNewProducer() throws Exception {
+
+        ProducerRequest request = new ProducerRequest("BMW", "123456", "bmw@mail.com", "99999");
+        ProducerResponse response = new ProducerResponse(1, "BMW", "123456", "bmw@mail.com");
+
+        when(addProducerService.createProducer(any())).thenReturn(response);
+
+        mockMvc.perform(post("/api/producers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "name": "BMW",
+                                  "phoneNumber": "123456",
+                                  "email": "bmw@mail.com",
+                                  "password": "99999"                                                                      
+                                                        }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.producerId").value(1))
+                .andExpect(jsonPath("$.name").value("BMW"))
+                .andExpect(jsonPath("$.phoneNumber").value("123456"))
+                .andExpect(jsonPath("$.email").value("bmw@mail.com"));
+
     }
 
     @Test
